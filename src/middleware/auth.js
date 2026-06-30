e147ef928c9b8653e59442f5477ccd7c44db75a8
Yaseen Saad Eldin <yaseensaadeldinmohamed@gmail.com>
2026-06-30 19:04:06 +0300

    Added Cookie parser and set up basic authentication

---
+++ src/middleware/auth.js
@@ -0,0 +1,42 @@
+const supabase = require('../lib/supabase');
+
+async function requireAuth(req, res, next) {
+    try {
+        const token = req.cookies?.sb_access_token || req.headers.authorization?.split('Bearer ', '');
+        if (!token) {
+            if (req.path.startsWith('/api/')) {
+                return res.status(401).json({ error: "Autehntication required" })
+            }
+            return res.redirect('/auth/login')
+        }
+        const { data: { user }, error } = await supabase.auth.getUser(token)
+        if (error || !user) {
+            if (req.path.startsWith('/api/')) {
+                return res.status(401).json({ error: "Invalid or expired token" })
+            }
+            return res.redirect('/auth/login')
+        }
+        req.user = user;
+        next();
+
+    }
+    catch (err) {
+        console.error("Auth error", err)
+        res.status(500).send("Auth Error")
+    }
+}
+
+async function optionalAuth(req, res, next) {
+    try {
+        const token = req.cookies?.sb_access_token || req.headers.authorization?.replace('Bearer ', '');
+        if (token) {
+            const { data: { user } } = await supabase.auth.getUser(token);
+            req.user = user || null;
+        }
+    } catch {
+        req.user = null;
+    }
+    next();
+}
+
+module.exports = { requireAuth, optionalAuth }; 
